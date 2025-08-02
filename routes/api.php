4e4531a1a7a038ffd4265ca69863763a8a89cfcc
ksazim <ks.azim1990@gmail.com>
2025-08-02 09:28:34 +0600

    Middleware implementation

--- routes/api.php
+++ routes/api.php
@@ -24,15 +24,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Authentication
 Route::post('login', [AuthController::class, 'login']);
-Route::get('logout', [AuthController::class, 'logout']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::get('sales-list/{paginate}/{customerId}/{productId}/{startDate}/{endDate}', [SalesController::class, 'list']);
-Route::post('create-sales', [SalesController::class, 'create']);
-Route::delete('delete-sale/{id}', [SalesController::class, 'destroy']);
+Route::get('sales-list/{paginate}/{customerId}/{productId}/{startDate}/{endDate}', [SalesController::class, 'list'])->middleware('auth:sanctum');
+Route::post('create-sales', [SalesController::class, 'create'])->middleware('auth:sanctum');
+Route::delete('delete-sale/{id}', [SalesController::class, 'destroy'])->middleware('auth:sanctum');
 
-Route::get('customers', [SystemController::class, 'customers']);
-Route::post('products', [SystemController::class, 'products']);
+Route::get('customers', [SystemController::class, 'customers'])->middleware('auth:sanctum');
+Route::post('products', [SystemController::class, 'products'])->middleware('auth:sanctum');
 
-Route::get('trash-list/{paginate}', [TrashController::class, 'list']);
-Route::delete('delete-trash-item/{id}', [TrashController::class, 'forceDelete']);
-Route::post('restore-trash-item', [TrashController::class, 'restore']);
+Route::get('trash-list/{paginate}', [TrashController::class, 'list'])->middleware('auth:sanctum');
+Route::delete('delete-trash-item/{id}', [TrashController::class, 'forceDelete'])->middleware('auth:sanctum');
+Route::post('restore-trash-item', [TrashController::class, 'restore'])->middleware('auth:sanctum');
